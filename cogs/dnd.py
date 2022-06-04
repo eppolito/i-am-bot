2f600469a42672d55d2c608ee769e453140b07bb
Chris Eppolito <eppolito@math.binghamton.edu>
2022-06-03 21:08:46 -0400

    Fix roll function to force arguments to match a regex.

--- cogs/dnd.py
+++ cogs/dnd.py
@@ -1,7 +1,8 @@
 from setup import *
 
 # random die rolls
-roll = lambda k,n: tuple(randint(1, n) for i in range(k))
+roll = lambda k, n: tuple(randint(1, n) for i in range(k))
+roll_regexp = "^(([1-9][0-9]?)d([1-9][0-9]?)(([\\+\\-])([1-9][0-9]?))?|adv|dis)$"
 
 class DnD(commands.Cog):
     '''DnD-related commands'''
@@ -38,41 +39,49 @@ class DnD(commands.Cog):
     async def roll_dice(self, ctx : Interaction, *args):
         '''Roll some dice.
 
-Takes multiple arguments of the form <k>d<n> to roll k n-sided die.
+Takes multiple arguments of the form <k>d<n> to roll k n-sided dice.
 Also parses advantage and disadvantage rolls as arguments <adv> and <dis>.
 
-This ignores all arguments that fail to match ([0-9]+d[0-9]+|adv|dis).
-It will error out if none of the arguments pass the parsing test.
+Arguments must match the following regular expression.
+      ^(([1-9][0-9]?)d([1-9][0-9]?)(([\\+\\-])([1-9][0-9]?))?|adv|dis)$
+Thus this function ignores all arguments greater than 99 (for Rhizka-proofing).
 '''
+        regex = re.compile(roll_regexp)
         ans = []
         pad_num = 3
         for arg in args:
-            if arg == "adv":
+            my_roll = regex.match(arg)
+            pad_num = max(pad_num, len(arg))
+            if not my_roll:
+                continue
+            [r, k, n, e, o, m] = my_roll.groups()
+            if r == "adv":
                 adv = roll(2, 20)
                 ans.append((arg, f"{max(adv)} = max{adv}"))
-                continue
-            elif arg == "dis":
+            elif r == "dis":
                 dis = roll(2, 20)
                 ans.append((arg, f"{min(dis)} = min{dis}"))
-                continue
             else:
-                try:
-                    [k, n] = arg.split('d')
-                    k, n = int(k), int(n)
-                    rs = roll(k, n)
-                    ans.append((arg, f"{sum(rs)} = sum{rs}"))
-                    pad_num = max(len(arg), pad_num)
+                my_roll = roll(int(k), int(n))
+                tot = sum(my_roll)
+                if o == "+":
+                    tot += int(m)
+                elif o == "-":
+                    tot -= int(m)
+                else:
+                    e = ""
                     pass
-                except ValueError:
-                    # ans_str += f"{arg}: `ParseError`\n"
-                    pass
+                ans.append((arg, f"{tot} = sum{my_roll}{e}"))
                 continue
             continue
         ans_string = ""
-        for (arg, s) in ans:
+        for (arg, st) in ans:
             pad = ' '*(pad_num - len(arg))
-            ans_string += f"{arg}:{pad} {s}\n"
-        await ctx.send(f"`{ans_string}`")
+            ans_string += f"{arg}:{pad} {st}\n"
+            continue
+        if bool(ans_string):
+            await ctx.send(f"```\n{ans_string}```")
+            return
         return
 
     # TODO: ADD A COMMAND TO QUERY THIS API: http://www.dnd5eapi.co/docs/#overview
